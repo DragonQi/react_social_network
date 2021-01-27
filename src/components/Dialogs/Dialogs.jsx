af567b968dff6a7bfe8ea8e27f706b017562d0b5
Roman DragonQi <pointlook987@gmail.com>
2021-01-27 11:05:24 +0500

    add simple button logic in dialogs and post

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -9,6 +9,13 @@ const Dialogs = (props) => {
     let messagesElements = props.state.messages
         .map((m) => <Message message={m.message}/>);
 
+    let newMessage = React.createRef();
+
+    let addMessage = () => {
+        let text = newMessage.current.value;
+        alert(text)
+    }
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -16,6 +23,12 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+            </div>
+            <div>
+                <textarea ref={newMessage}></textarea>
+            </div>
+            <div>
+                <button onClick={addMessage}>Message</button>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,16 +7,22 @@ const MyPosts = (props) => {
     let postsElements = props.posts
         .map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text)
+    };
 
     return (
         <div className={s.postsBlock}>
             <h3>my post</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add Post</button>
+                    <button onClick={addPost}>Add Post</button>
                 </div>
             </div>
                 <div className={s.posts}>
