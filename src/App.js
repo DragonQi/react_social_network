0f1117d65ad86d3d458571740b83044f010dc449
Roman DragonQi <pointlook987@gmail.com>
2021-01-31 23:48:37 +0500

    refactor and delete render.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route} from "react-router-dom";
-import {updateNewPostText} from "./redux/state";
 
 const App = (props) => {
     return (
