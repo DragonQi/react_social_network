10421d8d7edd5d33c2a83664a6da26a990dcf7b7
Roman DragonQi <pointlook987@gmail.com>
2021-01-31 23:46:20 +0500

    refactor and delete render.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import reportWebVitals from './reportWebVitals';
 import './index.css'
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
+import state, {subscribe} from "./redux/state";
+import React from 'react'
+import ReactDOM from "react-dom";
+import App from "./App";
+import {addPost, updateNewPostText} from "./redux/state";
+import {BrowserRouter} from "react-router-dom";
+
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state}
+                 addPost={addPost}
+                 updateNewPostText={updateNewPostText}
+            />
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 reportWebVitals();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () => {
+    console.log('state changed')
+}
 
 let state = {
 
@@ -9,7 +11,7 @@ let state = {
             {id: 3, message: "Working", likesCount: 15},
             {id: 4, message: "yo", likesCount: 13}
         ],
-        newPostText: ''
+        newPostText: 'lorem ipsum'
     },
     dialogsPage: {
         messages: [
@@ -44,7 +46,7 @@ let state = {
 
 };
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -55,9 +57,13 @@ export let addPost = () => {
     rerenderEntireTree(state);
 };
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
 };
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
+}
 
 export default state;
