7fe5c4346e284b6c7d4607be6ae2a4de6ded3bfe
Roman DragonQi <pointlook987@gmail.com>
2021-02-17 16:19:11 +0500

    add preloader.svg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Profile from "./components/Profile/Profile";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
-import Users from "./components/Users/Users";
 import {BrowserRouter, Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,20 +1,59 @@
 import React from 'react';
-import {connect} from "react-redux";
-import Users from "./Users";
+import {connect} from 'react-redux';
 import {
     followCreator,
     setCurrentPageCreator,
+    setToggleIsFetchingCreator,
     setTotalUsersCountCreator,
     setUsersCreator,
     unfollowCreator
 } from "../../redux/users-reducer";
+import axios from 'axios';
+import Users from './Users';
+import preloader from '../../assets/images/preloader.svg';
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        this.props.setToggleIsFetching(true);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setToggleIsFetching(false);
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        })
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        this.props.setToggleIsFetching(true);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setToggleIsFetching(false);
+            this.props.setUsers(response.data.items);
+        })
+    }
+
+    render() {
+        return <>
+            {this.props.isFetching ? <img src={preloader}/> : null}
+            <Users
+                totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChanged={this.onPageChanged}
+                users={this.props.users}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+            />
+        </>
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
         pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage
+        totalUsersCount: 100 /*state.usersPage.totalUsersCount*/, //принудительное ограничение, в связи с тем что пользователей больше 10 тыс
+        currentPage: state.usersPage.currentPage,
+        isFetching: state.usersPage.isFetching
     }
 };
 
@@ -34,7 +73,10 @@ let mapDispatchToProps = (dispatch) => {
         },
         setTotalUsersCount: (totalCount) => {
             dispatch(setTotalUsersCountCreator(totalCount))
+        },
+        setToggleIsFetching: (isFetching) => {
+            dispatch(setToggleIsFetchingCreator(isFetching))
         }
     }
 };
-export default connect (mapStateToProps, mapDispatchToProps)(Users);
+export default connect (mapStateToProps, mapDispatchToProps)(UsersContainer);
