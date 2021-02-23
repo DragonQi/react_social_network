e268639be222fcddecaf17d3952cce62f0806d6d
Roman DragonQi <pointlook987@gmail.com>
2021-02-23 14:08:33 +0500

    refactor + add follow unf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './App.css';
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import Music from "./components/Music/Music";
@@ -9,13 +8,14 @@ import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 const App = (props) => {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
-                <Header/>
+                <HeaderContainer/>
                 <Navbar state={props.state}/>
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs'
